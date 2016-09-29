60fdf43b8e95674548fa516ee35421f9a09377fb
Joris Borgdorff <joris@thehyve.nl>
2016-09-29 15:38:42 +0200

    Made an abstract KafkaMonitor for monitoring a stream

--- src/main/java/org/radarcns/process/BatteryLevelMonitor.java
+++ src/main/java/org/radarcns/process/BatteryLevelMonitor.java
@@ -3,90 +3,67 @@ package org.radarcns.process;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.generic.IndexedRecord;
-import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.radarcns.Device;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Properties;
 import java.util.Set;
 
-public class BatteryLevelMonitor {
-    private final String topic;
-    private final static Logger logger = LoggerFactory.getLogger(BatteryLevelMonitor.class);
-
+public class BatteryLevelMonitor extends KafkaMonitor {
     private final Set<Device> isLow;
     private final Set<Device> isCritical;
     private final List<BatteryLevelListener> listeners;
-    private final KafkaConsumer consumer;
 
     public BatteryLevelMonitor(String topic) {
-        Properties props = new Properties();
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-                io.confluent.kafka.serializers.KafkaAvroDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                io.confluent.kafka.serializers.KafkaAvroDeserializer.class);
-        props.put("schema.registry.url", "http://radar-test.thehyve.net:8081");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "battery_monitors");
-        props.put(ConsumerConfig.CLIENT_ID_CONFIG, "1");
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
-        props.put("bootstrap.servers", "radar-test.thehyve.net:9092");
-        this.topic = topic;
+        super(topic, "radar-test.thehyve.net:9092", "http://radar-test.thehyve.net:8081");
 
-        consumer = new KafkaConsumer<String, GenericRecord>(props);
-        consumer.subscribe(Collections.singletonList(topic));
+        Properties props = new Properties();
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "battery_monitors");
+        props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        configure(props);
 
         this.isLow = new HashSet<>();
         this.isCritical = new HashSet<>();
         this.listeners = new ArrayList<>();
     }
 
-    public void monitor() {
-        logger.info("Monitoring stream {}", topic);
+    protected void evaluateRecords(ConsumerRecords<String, GenericRecord> records) {
+        for (ConsumerRecord<String, GenericRecord> record : records) {
+            Device device = new Device(record.key());
+            IndexedRecord value = record.value();
+            Schema recordSchema = value.getSchema();
 
-        while (true) {
-            ConsumerRecords<String, GenericRecord> records = consumer.poll(Long.MAX_VALUE);
-            logger.info("Received {} records", records.count());
-            for (ConsumerRecord<String, GenericRecord> record : records) {
-                Device device = new Device(record.key());
-                IndexedRecord value = record.value();
-                Schema recordSchema = value.getSchema();
-
-                int batteryLevelFieldId = recordSchema.getField("batteryLevel").pos();
-                Number batteryLevel = (Number) value.get(batteryLevelFieldId);
-                if (batteryLevel.floatValue() < 0.05) {
-                    boolean newlyCritical = isCritical.add(device);
-                    if (newlyCritical) {
-                        isLow.add(device);
-                        for (BatteryLevelListener listener : listeners) {
-                            listener.batteryLevelStatusUpdated(device, BatteryLevelListener.Status.CRITICAL);
-                        }
+            int batteryLevelFieldId = recordSchema.getField("batteryLevel").pos();
+            Number batteryLevel = (Number) value.get(batteryLevelFieldId);
+            if (batteryLevel.floatValue() < 0.05) {
+                boolean newlyCritical = isCritical.add(device);
+                if (newlyCritical) {
+                    isLow.add(device);
+                    for (BatteryLevelListener listener : listeners) {
+                        listener.batteryLevelStatusUpdated(device, BatteryLevelListener.Status.CRITICAL);
                     }
-                } else if (batteryLevel.floatValue() < 0.2) {
-                    if (isLow.add(device)) {
-                        for (BatteryLevelListener listener : listeners) {
-                            listener.batteryLevelStatusUpdated(device, BatteryLevelListener.Status.LOW);
-                        }
+                }
+            } else if (batteryLevel.floatValue() < 0.2) {
+                if (isLow.add(device)) {
+                    for (BatteryLevelListener listener : listeners) {
+                        listener.batteryLevelStatusUpdated(device, BatteryLevelListener.Status.LOW);
                     }
-                } else {
-                    if (isLow.remove(device)) {
-                        isCritical.remove(device);
-                        for (BatteryLevelListener listener : listeners) {
-                            listener.batteryLevelStatusUpdated(device, BatteryLevelListener.Status.NORMAL);
-                        }
+                }
+            } else {
+                if (isLow.remove(device)) {
+                    isCritical.remove(device);
+                    for (BatteryLevelListener listener : listeners) {
+                        listener.batteryLevelStatusUpdated(device, BatteryLevelListener.Status.NORMAL);
                     }
                 }
             }
-
-            consumer.commitSync();
         }
     }
 
--- src/main/java/org/radarcns/process/KafkaMonitor.java
+++ src/main/java/org/radarcns/process/KafkaMonitor.java
@@ -1,7 +1,123 @@
 package org.radarcns.process;
 
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.errors.SerializationException;
+import org.radarcns.util.RollingTimeAverage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+import java.util.Properties;
+
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+
 /**
- * Created by joris on 29/09/2016.
+ * Monitor a single topic for anomalous behavior.
  */
-public class KafkaMonitor {
+public abstract class KafkaMonitor {
+    protected final String topic;
+    private final static Logger logger = LoggerFactory.getLogger(KafkaMonitor.class);
+
+    protected KafkaConsumer consumer;
+    private final Properties properties;
+
+    /**
+     * Set some basic properties.
+     *
+     * Update the properties field in the subclasses. During any overriding constructor, be sure
+     * to call {@see configure()}.
+     *
+     * @param topic topic to monitor
+     * @param kafkaServers string with Kafka bootstrap servers
+     * @param schemaUrl Schema Registry URL
+     */
+    public KafkaMonitor(String topic, String kafkaServers, String schemaUrl) {
+        properties = new Properties();
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, "1");
+        properties.setProperty("schema.registry.url", schemaUrl);
+        properties.setProperty("bootstrap.servers", kafkaServers);
+
+        this.consumer = null;
+        this.topic = topic;
+    }
+
+    /**
+     * Call to actually create the consumer.
+     */
+    protected void configure(Properties properties) {
+        consumer = new KafkaConsumer<String, GenericRecord>(properties);
+        consumer.subscribe(Collections.singletonList(topic));
+    }
+
+    /**
+     * Monitor a given topic until the {@see isDone()} method returns true.
+     *
+     * When a message is encountered that cannot be deserialized,
+     * {@see handleSerializationException} is called.
+     */
+    public void monitor() {
+        logger.info("Monitoring stream {}", topic);
+        RollingTimeAverage ops = new RollingTimeAverage(20000);
+
+        try {
+            while (!isDone()) {
+                try {
+                    ConsumerRecords<String, GenericRecord> records = consumer.poll(Long.MAX_VALUE);
+                    ops.add(records.count());
+                    logger.info("Received {} records", records.count());
+                    evaluateRecords(records);
+                    consumer.commitSync();
+                    logger.debug("Operations per second {}", (int) Math.round(ops.getAverage()));
+                } catch (SerializationException ex) {
+                    handleSerializationException();
+                }
+            }
+        } finally {
+            consumer.close();
+        }
+    }
+
+    /**
+     * Handles any deserialization message.
+     *
+     * This implementation tries to find the partition that contains the faulty message and
+     * increases the consumer position.
+     */
+    protected void handleSerializationException() {
+        logger.error("Failed to deserialize message. Skipping message.");
+        TopicPartition partition = null;
+        try {
+            Consumer<String, GenericRecord> tmpConsumer = new KafkaConsumer<>(properties);
+            for (Object partInfo : consumer.partitionsFor(topic)) {
+                partition = new TopicPartition(topic, ((PartitionInfo)partInfo).partition());
+                tmpConsumer.assign(Collections.singletonList(partition));
+                tmpConsumer.seek(partition, consumer.position(partition));
+                tmpConsumer.poll(0);
+            }
+        } catch (SerializationException ex1) {
+            consumer.seek(partition, consumer.position(partition) + 1);
+            return;
+        }
+        logger.error("Failed to find faulty message.");
+    }
+
+    /** Evaluate the records that the monitor receives by overriding this function */
+    protected abstract void evaluateRecords(ConsumerRecords<String, GenericRecord> records);
+
+    /**
+     * Whether the monitoring is done.
+     *
+     * Override to have some stopping behaviour, this implementation always returns false.
+     */
+    protected boolean isDone() {
+        return false;
+    }
 }
