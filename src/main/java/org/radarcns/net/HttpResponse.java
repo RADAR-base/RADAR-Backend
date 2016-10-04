14fa92feb28114ea7c9729ef1de9551ac629c97e
Joris Borgdorff <joris@thehyve.nl>
2016-10-04 14:57:37 +0200

    Turned off public/protected/private warnings: it's a library

---
+++ src/main/java/org/radarcns/net/HttpResponse.java
@@ -0,0 +1,7 @@
+package org.radarcns.net;
+
+/**
+ * Created by joris on 04/10/2016.
+ */
+public class HttpResponse {
+}
---
+++ src/main/java/org/radarcns/process/StreamToAvro.java
@@ -0,0 +1,7 @@
+package org.radarcns.process;
+
+/**
+ * Created by joris on 04/10/2016.
+ */
+public class StreamToAvro {
+}
