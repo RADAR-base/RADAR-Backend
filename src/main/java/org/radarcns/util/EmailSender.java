15969c8b37a061599ec6937c5b5c3cba4191c5c8
Joris Borgdorff <joris@thehyve.nl>
2017-01-17 17:46:31 +0100

    Removed unused import

--- src/main/java/org/radarcns/util/EmailSender.java
+++ src/main/java/org/radarcns/util/EmailSender.java
@@ -21,7 +21,6 @@ import java.util.List;
 import java.util.Properties;
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.NoSuchProviderException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
