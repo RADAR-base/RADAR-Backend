c1016957d45c420d191c25460eb626e67cb5cec4
Francesco Nobilia <f.nobilia@gmail.com>
2017-01-24 16:55:58 +0000

    Containers now check topic availability before starting RADAR component.
    Note: this check uses a Rest-Proxy API. In order to access the
    Rest-Proxy from inside connector’s container, the connector’s rest port
    must be different from the one used by Rest-Proxy.

---
+++ docker/radar-backend-kafka/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Busy waiting loop that waits untill all topic are available 
+echo "===> Waiting RADAR-CNS topics ... "
+./home/kafka_status.sh
+
+# Start streams
+echo "===> Starting streams ... "
+./usr/bin/java -jar /usr/share/java/radarbackend-0.1.jar -c /etc/radar.yml
---
+++ docker/radar-hdfs-connector/kafka_status.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Check if variables exist
+if [ -z "$KAFKA_REST_PROXY" ]; then
+        echo "KAFKA_REST_PROXY is not defined"
+        exit 126
+fi
+
+if [ -z "$TOPIC_LIST" ]; then
+        echo "TOPIC_LIST is not defined"
+        exit 126
+fi
+
+# Fetch env topic list
+IFS=', ' read -r -a needed <<< $TOPIC_LIST
+
+# Fetch env topic list
+count=0
+interval=1
+while [ "$count" != "${#needed[@]}" ] ; do
+
+    echo "Waiting $interval second before retrying ..."
+    sleep $interval
+    if (( interval < 30 )); then
+        ((interval=interval*2))
+    fi
+
+    count=0
+    TOPICS=$(curl -sX GET -H "Content-Type: application/json" "$KAFKA_REST_PROXY/topics")
+    TOPICS="$(echo -e "${TOPICS}" | tr -d '"'  | tr -d '['  | tr -d ']' | tr -d '[:space:]' )"
+
+    IFS=',' read -r -a array <<< $TOPICS
+    for topic in "${array[@]}"
+    do
+        for need in "${needed[@]}"
+        do
+            if [ "$topic" = "$need" ] ; then
+                ((count++))
+            fi
+        done
+    done
+done
+
+echo "All topics are now available. Ready to go!"
