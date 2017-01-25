58162a8b447a3307721f12b14428bd14298fcd3f
nivethika <nivemaham@gmail.com>
2017-01-25 12:44:48 +0100

    moved to docker related files to RADAR-Docker

--- docker/radar-backend-kafka/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Busy waiting loop that waits untill all topic are available 
-echo "===> Waiting RADAR-CNS topics ... "
-./home/kafka_status.sh
-
-# Start streams
-echo "===> Starting " $1 "...."
-./usr/bin/java -jar /usr/share/java/radarbackend-0.1.jar -c /etc/radar.yml $1
