0256f00db9a943a13db94fe5c79efe614045c3b6
Pim van Nierop <pim@thehyve.nl>
2026-02-24 13:27:50 +0100

    next

--- src/main/kotlin/org/radarbase/kotlin/config/RadarPropertyHandlerImpl.kt
+++ src/main/kotlin/org/radarbase/kotlin/config/RadarPropertyHandlerImpl.kt
@@ -16,8 +16,8 @@
 
 package org.radarbase.kotlin.config
 
-import org.radarbase.RadarBackend
 import org.radarbase.config.YamlConfigLoader
+import org.radarbase.kotlin.RadarBackend
 import org.radarbase.kotlin.util.PersistentStateStore
 import org.radarbase.kotlin.util.YamlPersistentStateStore
 import org.slf4j.LoggerFactory
